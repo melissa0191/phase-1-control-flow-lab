579fcb3da5fc4232fe095b93cde9f2dbbf5b1a5f
melissavelasquezgreene <melissa0191@outlook.es>
2023-11-18 16:45:56 -0500

    adds text

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(greeting){
+  if (greeting <= 400) {
+    return 'This one is on me!';
+
+  } else if (greeting >= 400 && greeting <= 2000) {
+    return 'That will be twenty bucks.';
+
+  } else if (greeting >= 2000 && greeting <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+
+  } else (greeting > 2500) 
+    return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+
+  let result = city === "NYC" ? "Ok, sounds good." : "No go."
+  
+  return result
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount) {
+  switch (tipAmount) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
+
 }
